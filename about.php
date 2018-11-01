682b2892b2f5074bd6168f2518c969d3ff13e289
MartyRizey <martyrizey@gmail.com>
2018-11-01 10:03:27 +0100

    etape 3 : controllers / 3 - déléguer la méthode show pour chaque page dans le controller et modifier les points d'entrés

--- about.php
+++ about.php
@@ -5,7 +5,17 @@
 
 
 
-// Affichage
-include __DIR__ .'/views/header.tpl.php';
-include __DIR__ .'/views/about.tpl.php'; // pour la page about
-include __DIR__ .'/views/footer.tpl.php';
+// Affichage ancienne méthode non dynamique
+// include __DIR__ .'/views/header.tpl.php';
+// include __DIR__ .'/views/about.tpl.php'; // pour la page about
+// include __DIR__ .'/views/footer.tpl.php';
+
+// Inclusion de la class MainController
+include __DIR__ . '/controllers/MainController.php';
+
+// Méthode dynamique
+// on instancie un objet de la classe 'MainController', cet objet utilisera les méthodes de cette classe.
+$controller = new MainController();
+
+// on appel ensuite la méthode home sur l'objet dans MainController.php gérant cette page
+$controller->about();
--- controllers/MainController.php
+++ controllers/MainController.php
@@ -9,35 +9,41 @@ class MainController
     // Méthode gérant le code de la page d'accueil
     public function home ()
     {
-        // ici la ligne générera une erreur quand on voudra afficher dans index.tpl.php $toto. La méthode 'show' ne connait pas cette variable.
         $toto = 'taratata';
         // J'appelle la méthode 'show' sur l'objet courant avec $this. Le 2iè argument est optionnel, car dans le paramètre de la méthode appelée on lui donne une valeur par défaut.
-        $this->show('index');
+        // Si je veux passer quelque chose à la méthode 'show' pour l'afficher, je peux utiliser un deuxième argument dans l'appel de la méthode. Puisque j'ai la possibilité que la méthode appelée est 2 paramètres. Je passe donc à $viewVars un tableau associatif.
+        // Délégue l'affichage à la méthode "show" du MainController"
+        $this->show('index', ['toto' => $toto]);
     }
 
     // Méthode gérant le code de la page 'about'
     public function about()
     {
-
+        // Délégue l'affichage à la méthode "show" du MainController"
+        $this->show('about');
     }
 
     // Méthode gérant le code de la page 'products'
     public function products()
     {
-
+        // Délégue l'affichage à la méthode "show" du MainController"
+        $this->show('products');
     }   
 
     // Méthode gérant le code de la page 'store'
     public function store()
     {
-
+        // Délégue l'affichage à la méthode "show" du MainController"
+        $this->show('store');
     }
 
     // video matin 2 : 1:16:00
     // Méthode s'occupant d'afficher une template (+ header et footer)
+    // celle-ci sera passée en 'private' car elle ne sera appelée qu'en interne, dans la classe du controller
     // Le 1er paramètre '$viewName' de la méthode show correspond à la vue qui va transiter ex : home, quand on va appeler la méthode en lui passanr un argument.
-    public function show($viewName, $viewVars=array()) {
-        var_dump($viewName);
+    private function show($viewName, $viewVars=array()) 
+    {
+        // le 2iè paramètre $viewVars récupère la valeur du 2iè argument de l'appel de la méthode, à savoir la valeur de $toto à la clé 'toto' du tableau associatif.
         // $viewVars est disponible dans chaque fichier de vue
         include __DIR__.'/../views/header.tpl.php';
         include __DIR__.'/../views/'.$viewName.'.tpl.php';
